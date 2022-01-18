33549bfa25995d2d19afe06d6c0301e8340c9813
Dali <oscardali370@gmail.com>
2022-01-18 09:38:45 -0600

    Basic explain

---
+++ Principal.py
@@ -0,0 +1,14 @@
+nombre = "Dali"
+edad = 20
+temperatura = 36.8
+sexo = 'H'
+
+print("El nombre es: ", nombre, " tiene ", edad, " años")
+print(f"El nombre es: {nombre} tiene {edad} años")
+
+n1 = int(input("Ingresa el n1: "))
+n2 = int(input("Ingresa el n2: "))
+
+suma = int(n1 + n2)
+
+print(f"La suma es {suma}")
