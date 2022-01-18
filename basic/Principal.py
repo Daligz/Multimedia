593e521ee7c593ccfcb40644c4cc35b26f42f7ac
Dali <oscardali370@gmail.com>
2022-01-18 10:09:18 -0600

    Object implemented

--- basic/Principal.py
+++ basic/Principal.py
@@ -1,8 +1,11 @@
+import Persona
+
 nombre = "Dali"
 edad = 20
 temperatura = 36.8
 sexo = 'H'
 
+"""
 print("El nombre es: ", nombre, " tiene ", edad, " años")
 print(f"El nombre es: {nombre} tiene {edad} años")
 
@@ -12,3 +15,14 @@ n2 = int(input("Ingresa el n2: "))
 suma = int(n1 + n2)
 
 print(f"La suma es {suma}")
+
+"""
+
+nombre = input("Nombre de la persona: ")
+edad = int(input("Edad de la persona: "))
+
+persona = Persona.Persona(nombre, edad)
+print(f"Datos de la persona, nombre {persona.getNombre()}, edad: {persona.getEdad()}")
+
+for i in range(5):
+    print(f"For ({i})", end=" | ")
