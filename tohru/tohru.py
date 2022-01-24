a874b9328d78f8b2d71cd28acb72f1e8895b9fe8
Dali <oscardali370@gmail.com>
2022-01-24 14:49:52 -0600

    Encapsulated code

--- tohru/tohru.py
+++ tohru/tohru.py
@@ -6,12 +6,14 @@ BROWN = (90, 50, 15)
 WHITE = (255,255,255)
 
 COLOR_SKIN = "#ffe7d5"
+COLOR_BORDER = "#651c0d";
+
+# Eyes
 COLOR_EYE = "#da3e49"
 COLOR_EYE_BRIGHTNESS = "#f0dc63";
 COLOR_EYE_BRIGHTNESS_BEHIND = "#f17e72";
 COLOR_EYE_BRIGHTNESS_TOP = "#fff7f8";
 COLOR_EYE_PUPIL = "#6f0a00";
-COLOR_EYE_BORDER = "#651c0d";
 
 pygame.init() 
 WIDTH = 800
@@ -21,8 +23,40 @@ Screen = pygame.display.set_mode(ScreenSize)
 pygame.display.set_caption("Tohru")
 
 # Functions
-def createCubeEntity(surface, x, y, color):
-    pygame.draw.rect(surface, color, (x, y, 60, 60))
+def printHead():
+    pygame.draw.circle(Screen, COLOR_BORDER, (400, 300), 272.5)
+    pygame.draw.circle(Screen, COLOR_SKIN, (400, 300), 270)
+
+def printLeftEye():
+    # Left eye border
+    pygame.draw.circle(Screen, COLOR_BORDER, (250,  250), 61.5)
+    # Left eye
+    pygame.draw.circle(Screen, COLOR_EYE, (250,  250), 60)
+    # Brightness behind eye
+    pygame.draw.ellipse(Screen, COLOR_EYE_BRIGHTNESS_BEHIND, [(217, 240), (70, 70)])
+    # Brightness eye
+    pygame.draw.ellipse(Screen, COLOR_EYE_BRIGHTNESS, [(235, 275), (50, 35)])
+    # Brightness top eye
+    pygame.draw.circle(Screen, COLOR_EYE_BRIGHTNESS_TOP, (225,  205), 17)
+    # Pupil eye
+    pygame.draw.ellipse(Screen, COLOR_EYE_PUPIL, [(245, 220), (10, 75)])
+
+def printRightEye():
+    # Right eye border
+    pygame.draw.circle(Screen, COLOR_BORDER, (530,  250), 61.5)
+    # Right eye
+    pygame.draw.circle(Screen, COLOR_EYE, (530,  250), 60)
+    # Brightness behind eye
+    pygame.draw.ellipse(Screen, COLOR_EYE_BRIGHTNESS_BEHIND, [(500, 240), (70, 70)])
+    # Brightness eye
+    pygame.draw.ellipse(Screen, COLOR_EYE_BRIGHTNESS, [(515, 275), (50, 35)])
+    # Brightness top eye
+    pygame.draw.circle(Screen, COLOR_EYE_BRIGHTNESS_TOP, (502,  208), 10)
+    # Brightness top eye two
+    pygame.draw.circle(Screen, COLOR_EYE_BRIGHTNESS_TOP, (542,  195), 5)
+    # Pupil eye
+    pygame.draw.ellipse(Screen, COLOR_EYE_PUPIL, [(525, 220), (10, 75)])
+
 
 endGame = False
 #Se define para poder gestionar cada cuando se ejecuta un fotograma
@@ -36,39 +70,12 @@ while not endGame:
     #---La lógica del juego
 
     #---Código de dibujo----
-    Screen.fill(BLACK)
+    Screen.fill(WHITE)
     #--Todos los dibujos van después de esta línea
 
-    # Head
-    pygame.draw.circle(Screen, COLOR_SKIN, (400, 300), 270)
-
-    # Left eye border
-    pygame.draw.circle(Screen, COLOR_EYE_BORDER, (250,  250), 61.5)
-    # Left eye
-    pygame.draw.circle(Screen, COLOR_EYE, (250,  250), 60)
-    # Brightness behind eye
-    pygame.draw.ellipse(Screen, COLOR_EYE_BRIGHTNESS_BEHIND, [(217, 240), (70, 70)])
-    # Brightness eye
-    pygame.draw.ellipse(Screen, COLOR_EYE_BRIGHTNESS, [(235, 275), (50, 35)])
-    # Brightness top eye
-    pygame.draw.circle(Screen, COLOR_EYE_BRIGHTNESS_TOP, (225,  205), 17)
-    # Pupil eye
-    pygame.draw.ellipse(Screen, COLOR_EYE_PUPIL, [(245, 220), (10, 75)])
-
-    # Right eye border
-    pygame.draw.circle(Screen, COLOR_EYE_BORDER, (530,  250), 61.5)
-    # Right eye
-    pygame.draw.circle(Screen, COLOR_EYE, (530,  250), 60)
-    # Brightness behind eye
-    pygame.draw.ellipse(Screen, COLOR_EYE_BRIGHTNESS_BEHIND, [(500, 240), (70, 70)])
-    # Brightness eye
-    pygame.draw.ellipse(Screen, COLOR_EYE_BRIGHTNESS, [(515, 275), (50, 35)])
-    # Brightness top eye
-    pygame.draw.circle(Screen, COLOR_EYE_BRIGHTNESS_TOP, (502,  208), 10)
-    # Brightness top eye two
-    pygame.draw.circle(Screen, COLOR_EYE_BRIGHTNESS_TOP, (542,  195), 5)
-    # Pupil eye
-    pygame.draw.ellipse(Screen, COLOR_EYE_PUPIL, [(525, 220), (10, 75)])
+    printHead()
+    printLeftEye()
+    printRightEye()
 
     #--Todos los dibujos van antes de esta línea
     pygame.display.flip()
